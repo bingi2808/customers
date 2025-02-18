47daf08a4c8ededc933a6d916abfcab630c38307
Bhuvana <bhuvanasai2808@gmail.com>
2025-02-18 14:28:48 -0800

    add service layer implementation

--- src/main/java/com/example/customers/service/CustomerServiceImpl.java
+++ src/main/java/com/example/customers/service/CustomerServiceImpl.java
@@ -1,23 +1,38 @@
 package com.example.customers.service;
 
 import com.example.customers.entity.CustomerEntity;
+import com.example.customers.exception.ResourceNotFoundException;
+import com.example.customers.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
-public class CustomerServiceImpl implements CustomerService{
-    @Override
-    public CustomerEntity createCustomer(CustomerEntity customerEntity) {
-        return null;
-    }
+@Service
+public class CustomerServiceImpl implements CustomerService {
 
-    @Override
-    public CustomerEntity getCustomerById(UUID id) {
-        return null;
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public List<CustomerEntity> getAllCustomers() {
-        return List.of();
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public CustomerEntity createCustomer(CustomerEntity customerEntity) {
+        if (customerRepository.findByEmail(customerEntity.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        return customerRepository.save(customerEntity);
+    }
+
+    @Override
+    public CustomerEntity getCustomerById(UUID id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
     }
 }
